815d70e76fac406be925731b111b5fbcf82c5da2
Helen <nikishina.olena@gmail.com>
2018-01-16 17:31:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,10 +6,9 @@ function getCart() {
  
 function addToCart(item) {
  const price = Math.floor(Math.random() * 100);
-  cart.push({ [item]: price});
+  cart.push({[item]: price});
  
-  console.log(`${item} has been added to your cart.`)
-
+  console.log(`${item} has been added to your cart.`);
   return cart;
 }
 
