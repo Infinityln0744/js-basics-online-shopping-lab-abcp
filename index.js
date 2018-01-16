9284355e48e8187b5473e4517a24f691a9e55295
Helen <nikishina.olena@gmail.com>
2018-01-16 17:03:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,8 +15,6 @@ function removeFromCart(){
 function placeOrder(cardNumber) {
  if (!cardNumber) {
    return console.log("Sorry, we don't have a credit card on file for you.")
-}
-
-  console.log(`Your total cost is $71, which will be charged to the card 83296759.`)
+ }
+ }
   cart = []
-}
