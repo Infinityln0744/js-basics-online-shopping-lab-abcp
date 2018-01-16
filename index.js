bb12cac90724c0333d34f2233bbd485275742260
Helen <nikishina.olena@gmail.com>
2018-01-16 16:58:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,8 +13,6 @@ return cart;
 }
 
 function viewCart(){
-  if (cart )
-  
 }
 
 function total(){
@@ -30,3 +28,4 @@ function placeOrder(cardNumber) {
 
   console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
   cart = []
+}
