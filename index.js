a9e342c83e8fed6a4b68793b5396c32fc6e5c2ee
Helen <nikishina.olena@gmail.com>
2018-01-16 16:02:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,30 +1,25 @@
 var cart = [];
 
-function getCart() {
- return cart;
+function getCart(){
+return cart;
 }
 
-function setCart(c) {
-  cart = c;
-  return cart;
+function addToCart(item){
+  const price = Math.floor(Math.random() * 100);
+    cart.push({ [item]: price});
+  
+console.log(`${item} has been added to your cart.`);
+return cart;
 }
 
-function addToCart(item) {
- // write your code here
+function viewCart(){
 }
 
-function viewCart() {
-  // write your code here
+function total(){
 }
 
-function total() {
-  // write your code here
+function removeFromCart(){
 }
 
-function removeFromCart(item) {
-  // write your code here
-}
-
-function placeOrder(cardNumber) {
-  // write your code here
+function placeOrder(){
 }
